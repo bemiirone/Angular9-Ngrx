dc8433cf4d17228806cbacba5da1ad30440e9776
Bemi Irone <bemidev@nsmultimedia.co.uk>
2020-05-15 09:27:59 +0100

    Auth store added

---
+++ src/app/auth/reducers/index.ts
@@ -0,0 +1,16 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+} from '@ngrx/store';
+
+export interface State {
+
+}
+
+export const reducers: ActionReducerMap<State> = {
+
+};
+
+
