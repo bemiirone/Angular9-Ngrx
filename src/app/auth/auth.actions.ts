ef46902c22d6dff056e419aff4a67b5c3e07e2a5
Bemi Irone <bemidev@nsmultimedia.co.uk>
2020-05-15 10:20:26 +0100

    auth actions added

---
+++ src/app/auth/auth.actions.ts
@@ -0,0 +1,8 @@
+import { User } from './model/user.model';
+import { createAction, props } from '@ngrx/store';
+
+
+export const login = createAction(
+  '[Login Page] User Login',
+  props<{user: User}>()
+);
